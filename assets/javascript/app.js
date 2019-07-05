e41f230c0f81b8d7656db50ca2977f8d62b1f32f
jenniferk2019 <51275153+jenniferk2019@users.noreply.github.com>
2019-07-05 16:56:21 -0400

    javascript functions simplified and edits made to match html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,75 +1,33 @@
-// // Variables
-// // Set the questions for the quiz with the answers
-// // Question 1
-// var myQuestions = [
-// {
-//     prompt: "What is Barbie's signature color?",
+// Variables
+// Set the questions for the quiz with the answers
 
-//     answers: {
-//         a: "Orange",
-//         b: "Pink",
-//         c: "Green",
-//     },
+function () {
 
-//     correctAnswer: "b"
-// },
-// // Question 2
-// {
-//     prompt: "Who is Barbie's boyfriend?",
+    var quest1 = document.quiz.quest1.value;
+    var quest2 = document.quiz.quest2.value;
+    var quest3 = document.quiz.quest3.value;
+    var quest4 = document.quiz.quest4.value;
+    
+// Questions user got correct
+    var score = 0;
 
-//     answers: {
-//         a: "Ken",
-//         b: "Voltron",
-//         c: "GIJoe",
-//     },
+// Setting [if] statments to confirm users response as correct or not
+if (quest1 == "Pink") {
+    score++; //score = score +1 - so if user gets this correct adds one point to score=0
+}
 
-//     correctAnswer: "a"
-// },
-// // Question 3
-// {
-//     prompt: "What job does Barbie have?",
+if (quest2 == "Ken") {
+    score++;
+}
 
-//     answers: {
-//         a: "Fashion Model",
-//         b: "President",
-//         c: "Both",
-//     },
+if (quest3 == "Both") {
+    score++;
+}
 
-//     correctAnswer: "c"
-// },
-// // Question 4
-// {
-//     prompt: "Where does Barbie live?",
+if (quest4 == "Both") {
+    score++;
+}
+}
 
-//     answers: {
-//         a: "Malibu",
-//         b: "New York",
-//         c: "Both",
-//     },
-
-//     correctAnswer: "c"
-// },
-
-// ];
-
-// // Variable
-// // Questions user got correct
-// var score = 0; 
-
-// // Loop through array
-// // Get user response
-// // Confirm users response as correct or not
-// // Alert user their answer is correct or not
-// for (var i=0; i < myQuestions.length; i++) {
-//     var response = window.prompt(myQuestions[i].prompt)
-// // If response is equal to current question then all is good
-//     if (response == myQuestions [i].answer) {
-// // Give an extra score when correct
-// score++;
-// alert ("Correct!");
-//     } else {
-//         alert ("Wrong!");
-//     }
-// }
-// // Score: Tell the user how many questions they got right out of the total
-// alert ("You got " + score + "/" + myQuestions.length);
+// Score: Tell the user how many questions they got right out of the total
+// alert("You got " + score + "/" + myQuestions.length);
