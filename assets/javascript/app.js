6689027866eeef512ec0981ff5916ae4ca91dec2
jenniferk2019 <51275153+jenniferk2019@users.noreply.github.com>
2019-07-05 21:15:29 -0400

    javascript timer functions update and some html codes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,18 @@
 // Variables
 // Set the question functions for the quiz with the answers
 
-function () {
+function quiz() {
 
     var quest1 = document.quiz.quest1.value;
     var quest2 = document.quiz.quest2.value;
     var quest3 = document.quiz.quest3.value;
     var quest4 = document.quiz.quest4.value;
+    var quest5 = document.quiz.quest5.value;
+    var quest6 = document.quiz.quest6.value;
+    var quest7 = document.quiz.quest7.value;
+    var quest8 = document.quiz.quest8.value;
+    var quest9 = document.quiz.quest7.value;
+    var quest10 = document.quiz.quest8.value;
 
     // Questions user got correct
     var score = 0;
@@ -27,11 +33,35 @@ function () {
     if (quest4 == "Both") {
         score++;
     }
+
+    if (quest5 == "House") {
+        score++;
+    }
+
+    if (quest6 == "Puppy") {
+        score++;
+    }
+
+    if (quest7 == "Sneakers") {
+        score++;
+    }
+
+    if (quest8 == "Pajamas") {
+        score++;
+    }
+
+    if (quest9 == "Yes") {
+        score++;
+    }
+
+    if (quest10 == "Yes") {
+        score++;
+    }
 }
 
 // Score: Tell the user how many questions they got right out of the total
 // alert("You got " + score + "correct";
-document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
+document.getElementById("number_correct").innerHTML = "You got " + score);
 
 // Start and Stop buttons
 $(document).ready(function(){
