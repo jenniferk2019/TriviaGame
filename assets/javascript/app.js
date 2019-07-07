e90c7877937c6277b42b74168d8f31d107bac8d8
jenniferk2019 <51275153+jenniferk2019@users.noreply.github.com>
2019-07-07 15:32:33 -0400

    new question array variable format

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,43 +1,90 @@
-// Timer object pulled from activity ten
+// Timer object pulled from activity 10
+var clockRunning = false;
 var timer = {
 
-  time: 15,
+    time: 15,
 
-  start: function() {
+    start: function () {
 
-    // DONE: Use setInterval to start the count here and set the clock to running.
-    if (!clockRunning) {
-      intervalId = setInterval(timer.count, 1000);
-      clockRunning = true;
+        // DONE: Use setInterval to start the count here and set the clock to running.
+        if (!clockRunning) {
+            intervalId = setInterval(timer.count, 1000);
+            clockRunning = true;
+            $("#timer").text(timer.time + " seconds")
+
+        }
+    },
+    stop: function () {
+        $("#submit").on("click", stop);
+        // DONE: Use clearInterval to stop the count here and set the clock to not be running.
+        clearInterval(intervalId);
+        clockRunning = false;
+
+
+        //     $("#number_correct").click(function(){
+        //       $("Congrats you got " + score + " correct").hide();
+        //     });
+        //     $("#show").click(function(){
+        //       $("p").show();
+        //     });
+        //   });
+    },
+
+    count: function () {
+
+        // DONE: increment time by 1.
+        timer.time--;
+        $("#timer").text(timer.time + " seconds")
+        // If timer is equal to 0 need to display current results to user
+        // Call end game
     }
-  },
-  stop: function() {
-
-    // DONE: Use clearInterval to stop the count here and set the clock to not be running.
-    clearInterval(intervalId);
-    clockRunning = false;
-  },
-  
-  count: function() {
-
-    // DONE: increment time by 1, remember we cant use "this" here.
-    timer.time--; 
-    $("#timer").text(timer.time + " seconds")
-  }
 
 };
 
-// Variables questions user got correct
-var quest1;
-var quest2;
-var quest3;
-var quest4;
-var quest5;
-var quest6;
-var quest7;
-var quest8;
-var quest9;
-var quest10;
+
+// Setting up variables for questions using Javascript YouTube video by Mike Dane Tutorial #24
+var questions = [
+    {
+        prompt: "What is Barbie's signature color?\n(a) Orange\n(b) Pink\n(c) Green",
+        answer: "b"
+    },
+    {
+        prompt: "Who is Barbie's boyfriend?\n(a) Ken\n(b) Voltron\n(c) Hulk",
+        answer: "a"
+    },
+    {
+        prompt: "What job does Barbie have?\n(a) Model\n(b) President\n(c) Both",
+        answer: "c"
+    },
+    {
+        prompt: "Where does Barbie live?\n(a) Malibu\n(b) New York\n(c) Both",
+        answer: "c"
+    },
+    {
+        prompt: "Barbie lives in a ____?\n(a) Condo\n(b) Dreamhouse\n(c) Shack",
+        answer: "b"
+    },
+    {
+        prompt: "What kind of pet does Barbie have?\n(a) Puppy\n(b) Fish\n(c) Parrot",
+        answer: "a"
+    },
+    {
+        prompt: "What would Barbie wear to exercise?\n(a) Heels\n(b) Sneakers\n(c) Both",
+        answer: "b"
+    },
+    {
+        prompt: "What would Barbie wear to a ball?\n(a) Jeans\n(b) Gown\n(c) Nothing",
+        answer: "b"
+    },
+    {
+        prompt: "Does Barbie like to dance?\n(a) No\n(b) Maybe\n(c) Yes",
+        answer: "c"
+    },
+    {
+        prompt: "Is Barbie famous?\n(a) Yes\n(b) No\n(c) Maybe",
+        answer: "a"
+    },
+]
 
 var score = 0;
 var intervalId;
@@ -100,80 +147,31 @@ function quiz() {
     if (quest10 == "Yes") {
         score++;
     }
-
+    console.log(score);
 }
 
 // Start and Stop buttons
-// start: function() {
-//     if (!clockRunning) {
-//         interv
-//     }
-// }
-
-// Score: Tell the user how many questions they got right out of the total
-// alert("You got " + score + "correct";
-// document.getElementById("number_correct").innerHTML = "You got " + score);
-// document.getElementById("number_correct").innerHTML = "You got " + score;
-
 // var timer= document.getElementById("timer").innerText;
 
-// Make sure document is ready before timer begins
-// Start and Stop buttons
-// $(document).ready(function(){
-//     $("#start").click(function(){
-      //How to link timer to begin button?
-//     });
-// $(document).ready(function () {
+$(document).ready(function () {
 
-//     $("#start").click(function () {
-//         setInterval(function(){
-//             timer = timer - 1
-//         console.log(timer)
+    $("#start").click(function () {
+        timer.start();
+    })
 
-//     $("#stop").click(function(){
-//       $("div").stop();
-//         }, 1000);
-        //if statment to make the timer stop at 0
-    // });
-
-// Stowatch timer variable and function to count down
-// var counter = 0;
-// var timeleft = 10;
-
-//         setInterval(function() {
-//             $('.Timer').text((10 - timeleft) / 1000 + " Seconds");
-//         }, 1000);
-//     $("#stop").click(function () {
-//         $("div").stop();
-//     });
+    $("#stop").click(function () {
+        timer.stop();
+        //if user clicks done call end game here as well - inside stop click function
+    })
 
 
+});
 
-
-
-// $(document).ready(function () {
-
-//     $("#start").click(function () {
-//         setInterval(function () {
-//             timer = timer - 1
-//         }, 1000);
-        
-//     });
-
-
-//     $("#stop").click(function () {
-//         if (timer) {
-//             timer = 0;
-//         ;
-//         }
-//     });
 
 
 // Score: Tell the user how many questions they got right out of the total
-// alert("You got " + score + "correct";
-// document.getElementById("number_correct").innerHTML = "You got " + score;
-
-//update the value inside the timer
+// .text("You got " + score + "correct"; - .hide for form and .show for results 
+//using jQuery make div to append results that will append to the html
 //take in the value of those radio buttons 
 //data tags for questions
 //once timer works use same syntax to let user know how many they got correct
