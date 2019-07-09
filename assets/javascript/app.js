10731bf5c7c860c9a21ea6fedf420cd362abe261
jenniferk2019 <51275153+jenniferk2019@users.noreply.github.com>
2019-07-09 13:18:44 -0400

    day off work on redo of javascript functions easy method and html with css updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,39 @@
-//submit the quiz function:
-function submitQuiz() {
+//submit the quiz function and check the answers:
+function check() {
+    var q1 = document.quizform.q1.value;
+    var q2 = document.quizform.q2.value;
+    var q3 = document.quizform.q3.value;
+    var q4 = document.quizform.q4.value;
+    var q5 = document.quizform.q5.value;
+    var q6 = document.quizform.q6.value;
+    var q7 = document.quizform.q7.value;
+    var q8 = document.quizform.q8.value;
 
-//when user selects submit button, the button should recognize each answer selected
-    
-
-//write a code that generates the calculated score with the function that establishses the users input
-
-
-//if the user answers all of the questions correctly, reveal "Congrats," which will be hidden in the meantime until user finishes the quiz
-
-//code to reveal score linked to the submitButton
-
-$(document).ready(function () {
-
-    $('#submitButton').click(function () {
-        $(this).addClass('hide');
-    });
-});
+    var count = 0;
+    //adding a point to the user's score when answering each question correctly:
+    if (q1 == "b") {
+        count++;
+    }
+    if (q2 == "a") {
+        count++;
+    }
+    if (q3 == "c") {
+        count++;
+    }
+    if (q4 == "c") {
+        count++;
+    }
+    if (q5 == "b") {
+        count++;
+    }
+    if (q6 == "a") {
+        count++;
+    }
+    if (q7 == "c") {
+        count++;
+    }
+    if (q8 == "c") {
+        count++;
+    }
+    alert("you got " +count+" correct");
+}
