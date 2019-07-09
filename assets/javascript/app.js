1128433abc5faa54f59f36203d1297ecd9aff09b
jenniferk2019 <51275153+jenniferk2019@users.noreply.github.com>
2019-07-08 21:34:53 -0400

    TA help with javascript code to fix for loop

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,12 +3,12 @@
 function submitQuiz() {
 
     //getting an answer for each question score
-    function answerScore (quizQuest) {
+    function answerScore(quizQuest) {
         var radioButton = document.getElementsByName(quizQuest);
 
         for (var i = 0, length = radioButton.length; i < length; i++) {
-               if (radiosButton[i].checked) {
-                var answerValue = Number(radiosButton[i].value);
+            if (radioButton[i].checked) {
+                var answerValue = Number(radioButton[i].value);
             }
         }
         if (isNaN(answerValue)) {
@@ -26,17 +26,18 @@ function submitQuiz() {
 
     var questCounter = 0;
 
-    for (var i = 0, length = questCount.length; i < length; i++) {
-        questionCounter++;
+    for (var i = 0; i < questCount[i].length; i < length; i++) {
+        var userChoice = questCount[i].value;
+        console.log(userChoice);;
     }
 
     //the "total-score"
     var totalScore = "Your Score: " + numScore;
 
     // if 8 / 8,
-if (numScore === questCounter) {
-            showScore = showScore
-        };
+    if (numScore === questCounter) {
+        showScore = showScore
+    };
 
     //html 'userScore' id
     document.getElementById('userScore').innerHTML = showScore;
